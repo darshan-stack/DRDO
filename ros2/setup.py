489e4b1f5bcbff6f6645a839ddca5504d11c38c3
Darshan Mistari <153345567+darshan-stack@users.noreply.github.com>
2026-09-12 11:15:52 +0530

    Install RViz configuration with ROS2 package

--- ros2/setup.py
+++ ros2/setup.py
@@ -4,13 +4,19 @@ from glob import glob
 package_name = 'ffem_lidar_mapping'
 setup(
     name=package_name,
-    version='0.1.0',
+    version='0.2.0',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name + '/launch', glob('launch/*.py')),
+        ('share/' + package_name + '/rviz', glob('rviz/*.rviz')),
     ],
     install_requires=['setuptools'],
-    entry_points={'console_scripts': ['ffem_node = ffem_lidar_mapping.node:main', 'ffem_rerun_logger = ffem_lidar_mapping.rerun_logger:main']},
+    entry_points={
+        'console_scripts': [
+            'ffem_node = ffem_lidar_mapping.node:main',
+            'ffem_rerun_logger = ffem_lidar_mapping.rerun_logger:main',
+        ]
+    },
 )
